4c3a21d00a99e52eef6ca54ed1f0fc7155c6d158
thinkPad <houhaopeng@outlook.com>
2020-03-11 20:24:22 +0800

    add ghost_net.py modified model.py

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from torch.nn import init
 from torchvision import models
 from torch.autograd import Variable
 import pretrainedmodels
+from ghost_net import ghost_net
 
 ######################################################################
 def weights_init_kaiming(m):
@@ -88,6 +89,36 @@ class ft_net(nn.Module):
         x = x.view(x.size(0), x.size(1))
         x = self.classifier(x)
         return x
+
+
+# Define the ghost_net-based Model
+class gn_net(nn.Module):
+
+    def __init__(self, class_num, droprate=0.5, stride=2):
+        super(gn_net, self).__init__()
+        model_gh = ghost_net(width_mult=1.0)
+        # avg pooling to global pooling
+        if stride == 1:
+            model_gh.layer4[0].downsample[0].stride = (1,1)
+            model_gh.layer4[0].conv2.stride = (1,1)
+        model_gh.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.model = model_gh
+        self.classifier = ClassBlock(2048, class_num, droprate)
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.model.avgpool(x)
+        x = x.view(x.size(0), x.size(1))
+        x = self.classifier(x)
+        return x
+
 
 # Define the DenseNet121-based Model
 class ft_net_dense(nn.Module):
